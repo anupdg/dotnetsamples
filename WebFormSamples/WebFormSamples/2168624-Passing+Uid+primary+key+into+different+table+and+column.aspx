e494807b8b7877fdef9be577ee3b7bbb98ba2e5d
Anup Das Gupta <anupdg@outlook.com>
2020-07-02 08:16:18 -0700

    Insert, get identity field and use in different command c# sql server

---
+++ WebFormSamples/WebFormSamples/2168624-Passing+Uid+primary+key+into+different+table+and+column.aspx
@@ -0,0 +1,15 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="2168624-Passing+Uid+primary+key+into+different+table+and+column.aspx.cs" Inherits="WebFormSamples._2168624_Passing_Uid_primary_key_into_different_table_and_column" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebFormSamples/WebFormSamples/2168624-Passing+Uid+primary+key+into+different+table+and+column.aspx.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormSamples
+{
+    public partial class _2168624_Passing_Uid_primary_key_into_different_table_and_column : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int userId = -1;
+
+            SqlConnection connection = new SqlConnection("connectionString");
+            // define query to be executed
+            string query = @"INSERT INTO user (Email, Password, ...)
+                              VALUES (@email, @password,......);
+                     SELECT SCOPE_IDENTITY();";
+
+            // set up SqlCommand in a using block   
+            using (SqlCommand objCMD = new SqlCommand(query, connection))
+            {
+                // add parameters using regular ".Add()" method 
+                objCMD.Parameters.Add("@email", SqlDbType.VarChar, 50).Value = "'aaaa@gmail.com'";
+                objCMD.Parameters.Add("@password", SqlDbType.VarChar, 100).Value = "pwd";
+                //...Other parameters
+                //..
+                // open connection, execute query, close connection
+                connection.Open();
+                object returnObj = objCMD.ExecuteScalar();
+
+                if (returnObj != null)
+                {
+                    int.TryParse(returnObj.ToString(), out userId);
+                }
+            }
+
+            if (userId > 0) {
+                query = @"INSERT INTO Wallet (Uid, amount) VALUES (@userId,@amount)";
+                using (SqlCommand objCMD = new SqlCommand(query, connection))
+                {
+                    // add parameters using regular ".Add()" method 
+                    objCMD.Parameters.Add("@userId", SqlDbType.Int, 50).Value = userId;
+                    objCMD.Parameters.Add("@amount", SqlDbType.Float, 100).Value = 0; //Change type here accordingly
+                    //...Other parameters
+                    //..
+                    // open connection, execute query, close connection
+                    connection.Open();
+                    object returnObj = objCMD.ExecuteScalar();
+
+                    if (returnObj != null)
+                    {
+                        int.TryParse(returnObj.ToString(), out userId);
+                    }
+
+                }
+            }
+            connection.Close();
+        }
+    }
+}
