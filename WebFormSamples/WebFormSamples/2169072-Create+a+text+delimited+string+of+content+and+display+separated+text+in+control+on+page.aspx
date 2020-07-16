8e0878ee09d36e3d8c0e4d48cdc1ff58e4373d85
Anup Das Gupta <anupdg@outlook.com>
2020-07-16 21:22:22 +0530

    Repeater - comma separated - add new

---
+++ WebFormSamples/WebFormSamples/2169072-Create+a+text+delimited+string+of+content+and+display+separated+text+in+control+on+page.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="2169072-Create+a+text+delimited+string+of+content+and+display+separated+text+in+control+on+page.aspx.cs" Inherits="WebFormSamples._2169072_Create_a_text_delimited_string_of_content_and_display_separated_text_in_control_on_page" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <asp:Repeater ID="rpt" runat="server">
+            <ItemTemplate>
+                <asp:TextBox runat="server"
+                    Text="<%# Container.DataItem %>" ReadOnly="true"></asp:TextBox><br />
+            </ItemTemplate>
+        </asp:Repeater>
+        <fieldset>
+            New content:
+            <asp:TextBox ID="txtNewData" runat="server"></asp:TextBox>
+            <asp:Button ID="btnAddNew" runat="server" Text="Add new" OnClick="btnAddNew_Click" />
+        </fieldset>
+    </form>
+</body>
+</html>
---
+++ WebFormSamples/WebFormSamples/2169072-Create+a+text+delimited+string+of+content+and+display+separated+text+in+control+on+page.aspx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Hosting;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebFormSamples
+{
+    public partial class _2169072_Create_a_text_delimited_string_of_content_and_display_separated_text_in_control_on_page : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                Session["Data"] = "Text1|Text2|Text3"; //set initial data
+                BindData();
+            }
+        }
+
+        private void BindData()
+        {
+            rpt.DataSource = GetData();
+            rpt.DataBind();
+        }
+        private List<string> GetData()
+        {
+            return Session["Data"].ToString().Split(new char[] { '|' }).ToList();
+        }
+        protected void btnAddNew_Click(object sender, EventArgs e)
+        {
+            var data = GetData();
+            data.Add(txtNewData.Text);
+            Session["Data"] = string.Join("|", data);
+            BindData();
+        }
+    }
+}
